9c8adbad59f7559a31704981dca5c48f51ac3f97
Sylaman <Sylaman.snake@gmail.com>
2022-08-20 14:07:18 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    // TODO создайте конструктор на все поля
+
+    // TODO создайте геттеры и сеттеры
+
+    // TODO создайте toString
+}
