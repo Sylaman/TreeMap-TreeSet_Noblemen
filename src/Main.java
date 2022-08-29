15ef4db845b2630aaf4501a5c4e095b9e6b8a67e
Sylaman <Sylaman.snake@gmail.com>
2022-08-29 21:12:02 +0300

    predicate developed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -9,10 +10,12 @@ public class Main {
 
         people.add(new Person("Egor", "Korshunov", 31));
         people.add(new Person("Anastasia", "Kunitskaya", 30));
+        people.add(new Person("Zaina", "Ali Zaki Ahmed Mohamed Ahmed", 16));
         people.add(new Person("Nikolay", "Rimsky Korsakov", 55));
         people.add(new Person("Dmitry", "Mamin Sibiryak", 60));
         people.add(new Person("Mohamed", "Ali Ahmed Yusuf Mohamed", 20));
-        people.add(new Person("Zaina", "Ali Zaki Ahmed Mohamed Ahmed", 16));
+        people.add(new Person("Nikolay", "Borisov", 10));
+
 
         Comparator<Person> comparator = (Person o1, Person o2) -> {
             String[] firstSurname = o1.getSurname().split(" ", maxWordsInSurname);
@@ -25,6 +28,9 @@ public class Main {
                 return Integer.compare(o1.getAge(), o2.getAge());
             }
         };
+
+        Predicate<Person> isAdult = (Person o1) -> o1.getAge() <= 18;
+        people.removeIf(isAdult);
 
         Collections.sort(people, comparator);
         Collections.reverse(people);
